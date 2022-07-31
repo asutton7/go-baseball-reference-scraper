7ddb1acd9dff86425f3c3843d0113d4f8ab8e8cc
Aaron Sutton <aaronsutton82@gmail.com>
2022-07-31 16:32:28 -0500

    Refactor server config to its own file

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,11 @@
 package main
 
 import (
-	s "baseball_scraper/internal/api/scrape_player"
-	"encoding/json"
-	"fmt"
+	cs "baseball_scraper/server"
 	"net/http"
 )
 
 func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/player", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r, w)
-		w.Header().Set("Content-Type", "application/json")
-		var requestJson struct{ Url string }
-
-		err := json.NewDecoder(r.Body).Decode(&requestJson)
-
-		if err != nil {
-			panic(err)
-		}
-		stats := s.ScrapePlayer(requestJson.Url)
-		fmt.Print(stats)
-		json.NewEncoder(w).Encode(stats)
-	})
-
+	mux := cs.ConfigureServer()
 	http.ListenAndServe(":8080", mux)
 }
